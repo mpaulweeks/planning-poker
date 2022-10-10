751aa0776c818fe5715a6127976ad58182f2128a
mpaulweeks <mpaulweeks@gmail.com>
2022-10-09 23:26:54 -0400

    updating + styles

--- src/pp/Room.tsx
+++ src/pp/Room.tsx
@@ -1,5 +1,6 @@
 import { useDB } from "./db";
 import { RoomInit } from "./types";
+import styles from './Room.module.css';
 
 export function Room(props: {
   init: RoomInit;
@@ -10,26 +11,74 @@ export function Room(props: {
     return <h1>loading {props.init.rid}</h1>;
   }
 
+  const users = Object.values(room.users);
+  console.log(users);
+
+  const voteNums = users.map(u => {
+    const { vote } = u;
+    if (!vote) { return NaN; }
+    const num = parseInt(vote);
+    return num;
+  }).filter(v => !isNaN(v));
+  const average = voteNums.reduce((sum, elm) => sum + elm, 0) / voteNums.length;
+
   return (
-    <>
-      <h1>
-        {room.rid}
-      </h1>
-      <div>
-        {room.options.map((vote, vi) => (
-          <button
-            key={vi}
-            style={vote === user.vote ? {
-              border: '2px solid green',
-            } : {}}
-            onClick={() => api.current.updateUser({
-              vote: vote === user.vote ? null : vote,
-            })}
-          >
-            {vote}
-          </button>
+    <div className={styles.Room}>
+      <header>
+        <div>
+          <b>{room.rid}</b>
+        </div>
+        <div className={styles.Self}>
+          <input value={user.name} onChange={evt => api.current.updateUser({
+            name: evt.target.value,
+          })} />
+          {room.options.map((vote, vi) => (
+            <button
+              key={vi}
+              style={vote === user.vote ? {
+                border: '2px solid green',
+              } : {}}
+              onClick={() => api.current.updateUser({
+                vote: vote === user.vote ? null : vote,
+              })}
+            >
+              {vote}
+            </button>
+          ))}
+        </div>
+      </header>
+      <section>
+        {users.map(user => (
+          <div key={user.uid}>
+            <div>
+              {user.name}
+            </div>
+            <div>
+              {!user.vote ? (
+                <i>pending</i>
+              ) : (
+                <b>
+                  {room.reveal ? user.vote : 'ready'}
+                </b>
+              )}
+            </div>
+          </div>
         ))}
-      </div>
-    </>
+      </section>
+      <footer>
+        {(room.reveal && average > 0) ? (
+          <div>
+            Average: {average.toFixed(1)}
+          </div>
+        ) : ''}
+        <div>
+          <button onClick={() => api.current.updateRoom({
+            reveal: !room.reveal,
+          })}>
+            {room.reveal ? 'HIDE': 'REVEAL'}
+          </button>
+        </div>
+      </footer>
+    </div>
   );
 }
