98e0526cdab7b2c4c0636994c1d062120b07e0a0
mpaulweeks <mpaulweeks@gmail.com>
2022-10-11 23:58:43 -0400

    move config to env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,14 @@
 import { FirebaseOptions } from "firebase/app";
 
+// React automatically includes .env
 export const FirebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyC6-Ia_K3-ZDkoxfLNUALK3SDHCeH4_0nw",
-  authDomain: "planning-poker-994c5.firebaseapp.com",
-  projectId: "planning-poker-994c5",
-  storageBucket: "planning-poker-994c5.appspot.com",
-  messagingSenderId: "481963696295",
-  appId: "1:481963696295:web:6aae9109390cb528207b82",
-  measurementId: "G-HJKY9SPZ1Y"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
+console.log(FirebaseConfig)
